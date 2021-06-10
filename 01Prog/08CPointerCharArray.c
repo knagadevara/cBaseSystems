71b80afc7e102692d23e7f64a8d5c399d3172e9d
knagadevara <vnsk.1991@gmail.com>
2021-06-11 00:25:40 +0530

    commented

--- 01Prog/08CPointerCharArray.c
+++ 01Prog/08CPointerCharArray.c
@@ -4,12 +4,12 @@
 char firstName[4] = {'S' , 'a' , 'i' ,'\0'}; // one cannot declare and initialize the charater type seperately it is a compiler error '\0' is a terminator and tells that there are no charactyers after this point
 char lastName[] = "Nagadevara" ; // This is known as a string literal and it dosent not reqire a '\0' to tell it is the end.
 
-int printFirstName(char* firstName)
+int printFirstName(char* pointerToCharArray)
     {
-        while (*firstName != '\0')
+        while (*pointerToCharArray != '\0')
         {
-            printf("firstName: %c. SizeOf: %ld\n", *firstName, sizeof(*firstName));
-            firstName = firstName + 1 ; // or firstName++ will add one unit size of character type to first name and saved it again. remember that thae evaluation happens from right to left
+            printf("firstName: %c. SizeOf: %ld\n", *pointerToCharArray, sizeof(*pointerToCharArray));
+            pointerToCharArray = pointerToCharArray + 1 ; // or firstName++ will add one unit size of character type to first name and saved it again. remember that thae evaluation happens from right to left
         }
         return 0;
     }
